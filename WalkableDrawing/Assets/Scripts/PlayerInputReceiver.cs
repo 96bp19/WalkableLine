4e1ff3241546186315614a12308126c1bfe5be10
96bp19 <lanthoo170@gmail.com>
2020-04-18 14:19:17 +0545

    Collision point added to the drawn line

---
+++ WalkableDrawing/Assets/Scripts/PlayerInputReceiver.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInputReceiver : MonoBehaviour
+{
+    public WalkablePoint walkablePointPrefab;
+
+    bool startUpdatingCollisionPoint;
+    Camera mainCam;
+
+    WalkablePoint currentUpdatableLine;
+    void Start()
+    {
+        mainCam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (startUpdatingCollisionPoint)
+        {
+            UpdateCollisionPointForLine();
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            startUpdatingCollisionPoint = true;
+            InitializeLine();
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            startUpdatingCollisionPoint = false;
+        }
+
+    }
+
+
+    void InitializeLine()
+    {
+        currentUpdatableLine = Instantiate(walkablePointPrefab, getMouseToWorldPos(), Quaternion.identity) as WalkablePoint;
+    }
+    void UpdateCollisionPointForLine()
+    {
+        currentUpdatableLine.AddNewCollisionPoint(getMouseToWorldPos());
+    }
+
+    Vector3 getMouseToWorldPos()
+    {
+        return mainCam.ScreenToWorldPoint(Input.mousePosition);
+    }
+}
---
+++ WalkableDrawing/Assets/Scripts/WalkablePoint.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(EdgeCollider2D), typeof(LineRenderer))]
+public class WalkablePoint : MonoBehaviour
+{
+    LineRenderer lineRenderer;
+    EdgeCollider2D edgeCollider;
+    List<Vector2> collisionPoints = new List<Vector2>();
+
+    private void Start()
+    {
+        collisionPoints = new List<Vector2>();
+
+        collisionPoints.Add(transform.InverseTransformPoint(transform.position));
+        collisionPoints.Add(transform.InverseTransformPoint(new Vector3(transform.position.x + 0.01f, transform.position.y, 0)));
+
+        lineRenderer = GetComponent<LineRenderer>();
+        edgeCollider = GetComponent<EdgeCollider2D>();
+        lineRenderer.positionCount = 2;
+        lineRenderer.SetPosition(0, transform.position);
+        lineRenderer.SetPosition(1, transform.position);
+        edgeCollider.points = collisionPoints.ToArray();
+    }
+
+
+
+    public void AddNewCollisionPoint(Vector2 position)
+    {
+
+        if (collisionPoints.Count > 0 && collisionPoints[collisionPoints.Count - 1] == position)
+        {
+            Debug.Log("same pos as before");
+            return;
+        }
+        collisionPoints.Add(transform.InverseTransformPoint(position));
+        lineRenderer.positionCount = collisionPoints.Count;
+
+        edgeCollider.points = collisionPoints.ToArray();
+        lineRenderer.SetPosition(collisionPoints.Count - 1, position);
+    }
+}
